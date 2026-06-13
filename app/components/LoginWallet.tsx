36adc20e3e84c34f78f9da22390b01c2008fd91c
USD Exchange <devjordan330@gmail.com>
2026-06-14 02:10:32 +0530

    Create LoginWallet.tsx

---
+++ app/components/LoginWallet.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LoginWallet() {
+  const [wallet, setWallet] = useState(null);
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert("MetaMask install karo");
+      return;
+    }
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    setWallet(accounts[0]);
+  };
+
+  return (
+    <div>
+      {!wallet ? (
+        <button onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      ) : (
+        <p>{wallet}</p>
+      )}
+    </div>
+  );
+}
