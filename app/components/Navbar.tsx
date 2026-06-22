f24db34869ba9e54160243f264af813089403e3b
USD Exchange <devjordan330@gmail.com>
2026-06-23 03:37:29 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,31 +1,28 @@
 "use client";
 
 import Link from "next/link";
-import LoginWallet from "../components/LoginWallet"; // <-- Is path ko perfect kar diya hai
+import LoginWallet from "./LoginWallet"; // Kyunki dono ek hi folder mein hain
 
 export default function Navbar() {
   return (
     <div style={{
       display: "flex",
-      gap: "15px",
-      padding: "12px 24px",
-      background: "#111",
+      padding: "15px 30px",
+      background: "#11141a",
       color: "#fff",
       alignItems: "center",
       justifyContent: "space-between",
-      borderBottom: "1px solid #222"
+      borderBottom: "1px solid #1f2937"
     }}>
-      {/* Links */}
       <div style={{ display: "flex", gap: "20px" }}>
-        <Link href="/dashboard" style={{ color: "#fff", textDecoration: "none", fontWeight: "500" }}>Dashboard</Link>
-        <Link href="/membership" style={{ color: "#fff", textDecoration: "none", fontWeight: "500" }}>Staking</Link>
-        <Link href="/rewards" style={{ color: "#fff", textDecoration: "none", fontWeight: "500" }}>Rewards</Link>
-        <Link href="/referral" style={{ color: "#fff", textDecoration: "none", fontWeight: "500" }}>Referral</Link>
-        <Link href="/profile" style={{ color: "#fff", textDecoration: "none", fontWeight: "500" }}>Profile</Link>
-        <Link href="/leaderboard" style={{ color: "#fff", textDecoration: "none", fontWeight: "500" }}>Leaderboard</Link>
+        <Link href="/dashboard" style={{ color: "#9ca3af", textDecoration: "none", fontWeight: "600" }}>Dashboard</Link>
+        <Link href="/membership" style={{ color: "#9ca3af", textDecoration: "none", fontWeight: "600" }}>Staking</Link>
+        <Link href="/rewards" style={{ color: "#9ca3af", textDecoration: "none", fontWeight: "600" }}>Rewards</Link>
+        <Link href="/referral" style={{ color: "#9ca3af", textDecoration: "none", fontWeight: "600" }}>Referral</Link>
+        <Link href="/profile" style={{ color: "#9ca3af", textDecoration: "none", fontWeight: "600" }}>Profile</Link>
+        <Link href="/leaderboard" style={{ color: "#9ca3af", textDecoration: "none", fontWeight: "600" }}>Leaderboard</Link>
       </div>
       
-      {/* Wallet Button */}
       <div>
         <LoginWallet />
       </div>
