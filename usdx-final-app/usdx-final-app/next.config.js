5620a6d942c3a1843e8c45bad80f436251477cb9
devjordan330 <devjordan330@gmail.com>
2026-07-01 22:38:21 +0530

    Dashboard UI progress and config updates

--- usdx-final-app/usdx-final-app/next.config.js
+++ usdx-final-app/usdx-final-app/next.config.js
@@ -1,11 +1,8 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-}
+  outputFileTracingRoot: path.join(__dirname),
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
